143000b42e7f1ff19bf28361597299d4019e89e1
BoramMoon <valeur00@gmail.com>
2017-01-27 10:11:32 +0900

    stack 1874 update, Queue basic 10845'

--- Boram/Queue_10845.java
+++ Boram/Queue_10845.java
@@ -1,39 +1,38 @@
 import java.util.*;
 
-
 public class Queue_10845 {
 
 	public static void main(String[] args) throws Exception {
-		/* 큐 구현하기	(Queue implement) */
+		/* 큐 구현하기 (Queue implement) */
+		// 시간 메모리 줄이는 방향으로 수정하기
 		Scanner input = new Scanner(System.in);
-		Queue<Integer> myQueue = new LinkedList<Integer>();
+		List <Integer> myQueue = new LinkedList<Integer>();
 		int count = input.nextInt();
-		String str; 
-		int back =0 ;
-		
+		String str;
+
 		for (int i = 0; i < count; i++) {
 			str = input.next();
 			switch (str) {
 			case "push":
 				int push_x = Integer.parseInt(input.next());
-				myQueue.add(push_x); back = push_x;
+				myQueue.add(push_x);
 				break;
 			case "pop":
-				try{
-				if (myQueue.isEmpty()) {
-					System.out.println(-1);
-				} else {
-					System.out.println(myQueue.peek());
-					myQueue.remove(); 
-					back = myQueue.peek();
-					if(myQueue.isEmpty()){ back = 0; }
+				try {
+					if (myQueue.isEmpty()) {
+						System.out.println(-1);
+					} else {
+						System.out.println(myQueue.get(0));
+						myQueue.remove(0);
+					}
+				} catch (Exception e) {
+					e.toString();
 				}
-				}catch(Exception e){ e.toString(); }
 				break;
 			case "size":
 				System.out.println(myQueue.size());
 				break;
-			case "empty":				
+			case "empty":
 				if (myQueue.isEmpty()) {
 					System.out.println(1);
 				} else {
@@ -44,14 +43,14 @@ public class Queue_10845 {
 				if (myQueue.isEmpty()) {
 					System.out.println(-1);
 				} else {
-					System.out.println(myQueue.peek());
+					System.out.println(myQueue.get(0));
 				}
 				break;
 			case "back":
 				if (myQueue.isEmpty()) {
 					System.out.println(-1);
 				} else {
-					System.out.println(back);
+					System.out.println(myQueue.get(myQueue.size()-1));
 				}
 				break;
 			}
--- Boram/Stack_1874.java
+++ Boram/Stack_1874.java
@@ -3,34 +3,45 @@ import java.util.*;
 
 public class Stack_1874 {
 
-	public static void main(String[] args) throws IOException{
-		Scanner input = new Scanner(System.in);
-		int input_count = input.nextInt();
+	public static void main(String[] args) throws IOException {
+		// 스택 수열 _ Error_ 출력초과 에러 : int배열로 스택 만들어 보기
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		int input_count = Integer.parseInt(input.readLine());
 		int number, max = 1;
 		Stack<Integer> myStack = new Stack<Integer>();
-		
-		for (int i = 0; i < input_count; i++){
-			number = input.nextInt();
-			try{
-			if(max < number){
-				for(int j = max; j<= number; j++){
-					myStack.push(j);
-					System.out.println("+");	
+		//StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < input_count; i++) {
+			number = Integer.parseInt(input.readLine());
+			try {
+				if (max <= number) {
+					for (int j = max; j <= number; j++) {
+						myStack.push(j);
+						System.out.println("+");
+						//sb.append("+\n");
+					}
+					max = number + 1;
+					myStack.pop();
+					System.out.println("-");
+					//sb.append("-\n");
 				}
-				max = number+1;
-			}
-			
-			if(myStack.peek()!=number) {System.out.println("NO");}
-			
-			else if(myStack.peek()==number){
-				myStack.pop();
-				System.out.println("-");
-			}
-					
-			}catch(EmptyStackException e){
+
+			    else if (myStack.peek() != number) {
+					System.out.println("NO");
+					//sb.append("NO\n");
+				}
+                
+                else{
+                    myStack.pop();
+                    System.out.println("-");
+                    //sb.append("-\n");
+                }
+
+			} catch (EmptyStackException e) {
 				e.toString();
 			}
-		}		
+		}
+		//System.out.println(sb);
 	}
 
 }
