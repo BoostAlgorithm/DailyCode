673358d4c50b54a95645a4b1e0fde3042ca8a967
BoramMoon <valeur00@gmail.com>
2017-01-27 10:14:07 +0900

    Queue basic update

--- Boram/Queue_10845.java
+++ Boram/Queue_10845.java
@@ -4,9 +4,13 @@ public class Queue_10845 {
 
 	public static void main(String[] args) throws Exception {
 		/* 큐 구현하기 (Queue implement) */
-		// 시간 메모리 줄이는 방향으로 수정하기
+		/* 제출 성공, 시간  메모리 줄이는 방향으로 수정하기 */
 		Scanner input = new Scanner(System.in);
+		// Queue <Integer> myQueue = new LinkedList<Integer>();
+		// offer(e), poll(), peek() : Queue 인터페이스 함수 , false (add의 경우) or null 반환
+		// add(e), remove(), element() : Collections 인터페이스 함수 , Exception 발생
 		List <Integer> myQueue = new LinkedList<Integer>();
+		// back의 원소를 확인하기 위해서 List 인터페이스를 사용함. list.get(index);
 		int count = input.nextInt();
 		String str;
 
@@ -57,4 +61,5 @@ public class Queue_10845 {
 
 		}
 	}
+	// 시간 복잡도  O(n), 공간복잡도  S(n)
 }
