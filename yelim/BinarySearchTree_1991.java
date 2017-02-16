f777437cc0342e829e5b7eeed59d8cac4ab061d9
LeeYelim <pkmarin15@naver.com>
2017-02-16 21:04:01 +0900

    170216 21:03 bst

--- yelim/BinarySearchTree_1991.java
+++ yelim/BinarySearchTree_1991.java
@@ -52,7 +52,6 @@ class NodeDTO {
 public class BinarySearchTree_1991 {
 
 	public static void main(String[] args) throws IOException {
-		
 		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		//int n = Integer.parseInt(br.readLine()); // 이진 트리 노드의 갯수
 		Scanner sc = new Scanner(System.in);
