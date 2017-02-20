a96f47a3ef4458fcdec92ec05bb549f3f9e8b405
LeeYelim <pkmarin15@naver.com>
2017-02-20 15:37:58 +0900

    170220 bufferReader사용

--- yelim/StackSequence_1874.java
+++ yelim/StackSequence_1874.java
@@ -1,41 +1,47 @@
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 
 public class StackSequence_1874 {
-
-	public static void main(String[] args) throws IOException {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int data;
+	/**
+	 * 1. 들어온 값이 top보다 크면
+	 * 이전 top부터
+	 * 해당 숫자가 될 때까지 push하고 도달하면 pop(해당값-top 만큼)
+	 * top을 들어온 값으로 
+	 * 2. top보다 작으면 
+	 * top부터 해당 값까지 pop(top-해당 값 만큼)
+	 * top보다 작은 해당값은 항상 top-1밖에 될 수 없음
+	 * 	: 
+	 * */
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int num;
+		int top=0;
 		int index=0;
-		int max=0; // 입력된 값들의 최대값
-		int stack[] = new int[100000];
-		StringBuilder sb = new StringBuilder();
-
-
+		int stack[] = new int[n];
+		StringBuffer bf = new StringBuffer();
+		
 		for(int i=0; i<n; i++) {
-			data = sc.nextInt();
-			if(data>max) {
-				// max 다음 값 부터 data까지 스택에 push하고 pop
-				for(int j=max+1; j<=data; j++) {
-					stack[index++]=j;
-					sb.append("+\n");
+			num = Integer.parseInt(br.readLine());
+			if(num>top) {
+				for(int j=top+1; j<=num; j++) {
+					stack[index++] = j;
+					bf.append("+\n");
 				}
-			} else 
-				if(stack[index-1]!=data) {
+			} else {
+				if(stack[index-1] != num) {
 					System.out.println("NO");
 					return;
 				}
+			}
 			index--;
-			sb.append("-\n");
-			if (data > max) max = data;
-			
+			bf.append("-\n");
+			if(num>top) top=num;
 		}
-		System.out.println(sb);
-
-
+		
+		System.out.println(bf);
 	}
+
 }
-
-
-
