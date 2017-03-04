1a0a62f4d3f1ad6be8ab0b6650dd058f1705fb68
LeeYelim <pkmarin15@naver.com>
2017-03-05 02:07:37 +0900

    수열의 합 170305 02:07 실패이유찾기

---
+++ yelim/SumofSequence_1024.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class SumofSequence_1024 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str);
+		int n = Integer.parseInt(st.nextToken()); // 합 N
+		int l = Integer.parseInt(st.nextToken()); // N을 이루는 자연수의 최소 길이
+		
+		getSumOfSequence(n, l);
+ 	}
+
+	/**
+	 * 15649836 16
+	 * 18 / 3 = 6 (5, 6, 7)
+	 * 19 / 2 = 9 (9, 10) 
+	 * 25 / 5 = 5 (3, 4, 5, 6, 7)
+	 * 등차 수열 합
+	 * 1~n까지 연속된 수의 합 = n(n+1)/2
+	 * */
+	private static void getSumOfSequence(int n, int l) {
+		
+		for(int i=l; i<100; i++) {
+			int end = n/i + i/2; 
+			int start = end-i+1;
+			
+			if(start <0 || end>n) {
+				continue;
+			} else if(n == end*(end+1)/2 - start*(start-1)/2) {
+				for(int j=start; j<=end; j++) {
+					System.out.print(j + " ");
+				}
+				return;
+			}
+		}
+		System.out.println(-1);
+	}
+
+}
